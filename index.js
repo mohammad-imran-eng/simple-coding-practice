d9e4e170c9942ef0b8e364cbb8f415a0f2d26459
Mohammad Imran <immu.engineer@gmail.com>
2024-09-06 00:49:23 +0600

    simple js coding problem solving

---
+++ index.js
@@ -0,0 +1,41 @@
+// const number = [1,2,3,4,5,6,7,8,9];
+
+// console.log(number)
+
+// number[0] = 100;
+
+
+// console.log(number);
+
+
+// arry is mutable
+// string is immutable
+
+
+// const add = 'I am a student of web app development.'
+
+//     console.log(add);
+
+//   const newAdd = add.split(',')
+
+//   console.log(newAdd);
+
+//   const first = 'Abid';
+//   const last = 'Islam'
+
+//   const fullName = first.concat(' ').concat(last)
+
+//   console.log(fullName)
+
+
+// const sentence = 'developer';
+ 
+// let reverse = ''
+
+// for (const letter of sentence){
+//   console.log(letter)
+//   // ved
+//   reverse = letter + reverse;
+// }
+
+// console.log(reverse)
