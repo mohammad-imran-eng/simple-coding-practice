60ba5ee958463f2cf600ec0d918790c643453636
Mohammad Imran <immu.engineer@gmail.com>
2024-09-07 03:31:20 +0600

    problem solving with js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@
 
 // const sentence = 'developer';
  
-// let reverse = ''
+// // let reverse = ''
 
 // for (const letter of sentence){
 //   console.log(letter)
@@ -39,3 +39,66 @@
 // }
 
 // console.log(reverse)
+
+
+// const sentence = 'developer';
+ 
+// let reverse = ''
+
+
+// for (let i = 0; i<sentence.length; i++){
+//   console.log(sentence[i])
+//   const letter = sentence[i]
+//   reverse = letter + reverse
+// }
+
+// console.log(reverse)
+
+
+// //shortcut 
+
+// const rev = sentence.split('').reverse().join('')
+
+// console.log(rev)
+
+// const mobile = {
+//   brand: 'samsung',
+//   price: '212222',
+//   color: 'black',
+//   camera: '12MP'
+// }
+
+// for (const prop in mobile){
+//   console.log(mobile[prop])
+// }
+
+
+const persons = ['rakib','jakib','habib','nadib','kadib','radib'];
+
+console.log(persons.sort());
+
+// Ascending ------> smaller to larger: []
+
+// Descending ------> larger to smaller: []
+
+
+const newFuncion=(value)=> {
+    console.log(value)
+}
+
+newFuncion(true)
+
+
+//Objective: write a function to give me the sum of all numbers in an array
+
+
+let sum = 0;
+const problem = (data)=> {
+    for(let i= 0; i<data.length; i++){
+         sum = sum + data[i];
+    }
+    console.log(sum)
+}
+
+
+problem([10,20,30,40])
