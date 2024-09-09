fad3908eae8fcc92ade80d8a331deeb3046de4fb
Mohammad Imran <immu.engineer@gmail.com>
2024-09-10 01:01:20 +0600

    commit new problem solve

--- index.js
+++ index.js
@@ -73,20 +73,22 @@
 // }
 
 
-const persons = ['rakib','jakib','habib','nadib','kadib','radib'];
+// const persons = ['rakib','jakib','habib','nadib','kadib','radib'];
 
-console.log(persons.sort());
+// console.log(persons.sort());
 
 // Ascending ------> smaller to larger: []
 
 // Descending ------> larger to smaller: []
 
 
-const newFuncion=(value)=> {
-    console.log(value)
-}
+// const newFuncion=(value)=> {
+//     console.log(value)
+// }
 
-newFuncion(true)
+// newFuncion(true)
+
+
 
 
 //Objective: write a function to give me the sum of all numbers in an array
@@ -104,21 +106,115 @@ newFuncion(true)
 // problem([10,20,30,40])
 
 
-const evenNumber = []
+// const evenNumber = []
 
-const problem = (data)=> {
-    for(let i=0; i<data.length; i++){
-        if(data[i] % 2 === 0){
-           evenNumber.push(data[i])
-        }
+// const problem = (data)=> {
+//     for(let i=0; i<data.length; i++){
+//         if(data[i] % 2 === 0){
+//            evenNumber.push(data[i])
+//         }
+//     }
+//     return evenNumber;
+
+// }
+
+
+
+// returnValue = problem([10,20,30,43]);
+
+
+// console.log(returnValue)
+
+
+
+// how to remove unique value an array
+
+// const friends = ['abdullah','abdullah','imran','robiul','radib'];
+
+// let newFriend = [];
+// const removeDouble = ()=> {
+//     for(const friend of friends){
+//         if(!newFriend.includes(friend)){
+//             newFriend.push(friend)
+//         }
+//     }
+//     return newFriend;
+// }
+
+
+// const result = removeDouble()
+
+// console.log(result)
+
+
+// const day = Date();
+
+// console.log(day)
+
+
+// //Write a function to convert temperature celcius to farenheit
+
+
+// let f = 0;
+
+// const convert = (c)=> {
+//     f = (c*1.8) + 32
+//     return f;
+// }
+
+// const result = convert(40)
+// console.log(result)
+
+
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+// const numbers = [1,1,2,2,3,4,5,6,7,8,9,9]
+// const newArray = []
+// let sum = 0;
+
+// const repeated =()=> {
+
+//    for(number of numbers){
+//     if(!newArray.includes(number)){
+//         newArray.push(number)
+//     }
+//     else {
+//          sum = sum + 1
+//     }
+//    }
+
+//    return sum;
+   
+    
+// }
+
+
+//  const result = repeated();
+
+//  console.log(result)
+
+
+
+// for (var i = 0; i < 5; i++) {
+//     setTimeout(function () {
+//     console.log(i);
+//  }, 100);
+// }
+
+
+// check 3 value what is the bigger
+
+const bigger = (one,two,three)=> {
+    if(one > two && one > three){
+        console.log(one)
     }
-    return evenNumber;
-
+    else if(two > one && two > three){
+        console.log(two)
+    }
+    else {
+        console.log(three)
+    }
 }
 
-
-
-returnValue = problem([10,20,30,43]);
-
-
-console.log(returnValue)
+bigger(1,2,3)
+bigger(10,20,30)
