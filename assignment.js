f45baeaaa2760d989189a8b0a1bf50b77bcd2f2d
Mohammad Imran <immu.engineer@gmail.com>
2024-09-11 06:25:49 +0600

    solve three problem

--- assignment.js
+++ assignment.js
@@ -1,21 +1,71 @@
 // first problem solve
 
 
-function calculateTax(income, expenses) {
+// function calculateTax(income, expenses) {
    
-    const tax = (income - expenses) * .20
+//     const tax = (income - expenses) * .20
 
-    if(income && expenses < 0){
-        return "Invalid Input"
-    }
-    else if(income < expenses ){
-        return "Invalid Input"
-    }
+//     if(income && expenses < 0){
+//         return "Invalid Input"
+//     }
+//     else if(income < expenses ){
+//         return "Invalid Input"
+//     }
 
-    return tax;
+//     return tax;
 
-}
+// }
 
 
-const result = calculateTax(1000,2000);
-console.log(result)
+// const result = calculateTax(1000,2000);
+// console.log(result)
+
+
+
+// second problem solve
+
+// function sendNotification(email) {
+//    let result = email.split("@",1)
+//    let resultTwo = email.split("@");
+//    let firstName = result.toString()
+//    if(!email.includes('@')){
+//         return "Invalid Email";
+//    }
+//    else if(email.includes("@")){
+//     return firstName + " sent you an email from  " + resultTwo[1].toString()
+//    }
+   
+// }
+
+
+// Fourth problem solve
+
+// function calculateFinalScore(obj) {
+
+//     let totalScore = obj.testScore + obj.schoolGrade;
+   
+//    if(obj.isFFamily){
+//     totalScore = totalScore + 20;
+
+//    }
+//     if(!obj.isFFamily){
+//     totalScore = totalScore;
+    
+//    }
+
+//    if(typeof obj !== 'object'){
+//     return "Invalid Input"
+//    }
+
+
+//     if(totalScore >=80){
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+    
+
+
+// }
+
