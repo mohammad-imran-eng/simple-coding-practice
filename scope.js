e8ea8d2bce9494cd8436fbe92a258aad50d2e0f0
Mohammad Imran <immu.engineer@gmail.com>
2024-10-02 01:37:35 +0600

    Practice ES6

---
+++ scope.js
@@ -0,0 +1,59 @@
+// const manager = (gretting,name)=> {
+//     gretting(name)
+// }
+
+
+// const gretting = (name)=> {
+//     console.log('Good Morning', name);
+// }
+
+// const grettingEvening = (name)=> {
+//     console.log('Good Evening', name);
+// }
+
+// const greetNignt = (name)=> {
+//     console.log('Good Night', name);
+// }
+
+// manager(gretting,'Mohammad Imran');
+// manager(gretting,'Sirajam Munira ');
+// manager(grettingEvening, 'Rifad Islam');
+
+
+// const text =()=> {
+//      x = 10; // global variable declare
+//     console.log(x);
+// }
+
+// text()
+
+// console.log(x)
+
+
+
+// const sum = x => x = x * x;
+
+// const result = sum(5);
+
+// console.log(result)
+
+
+// const defaultPerameter = (a,b,c=10)=> a+b+c;
+
+// console.log(defaultPerameter(10,10));
+
+// const x =10;
+
+// console.log(x === 5 ? "value is Five" : "value is not Five");
+
+
+// const arr1 = [1,2,3];
+// const arr2 = [...arr1]; spread operator
+ 
+// arr1.push(4);
+// console.log(arr1,arr2);
+
+
+// const [_,y,z] = [10,20,30]; array destructure
+
+// console.log(y)
